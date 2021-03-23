e6e0d98c3198295c1fce0e5d455443b423edfa09
Hunter Foist <hfoist@gmail.com>
2021-03-23 19:46:16 -0400

    props mapping

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PropsExample from './components/PropsExample'
+import PropsMappingExample from './components/PropsMapping'
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ['Alabama', 'Kentucky', 'Maryland', 'Ohio', 'Michigan', 'Illinois']
+  
+  return <div>
+    <PropsExample name="Tom" business="MySpace"/>
+    <PropsMappingExample visited={visitedPlaces}/>
+  </div>;
 }
 
 export default App;
